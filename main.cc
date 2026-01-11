5c267588d95a1e942e53080b0210fc6de07daf90
Daniel Alp <daniel.alp.email@gmail.com>
2026-01-11 09:24:20 -0500

    check signature

--- main.cc
+++ main.cc
@@ -1,18 +1,40 @@
+template<typename T, typename U>
+struct is_same {
+    static constexpr bool value = false;
+};
+
+template<typename T>
+struct is_same<T, T> {
+    static constexpr bool value = true;
+};
+
 template<typename>
 struct FunctionArity {};
 
 template<typename R, typename... Args>
-struct FunctionArity <R(Args...)>
-{
+struct FunctionArity<R(Args...)> {
+    static_assert(is_same<R, int>::value, "return type must be int");
+    static_assert((is_same<Args, int>::value && ...), "parameter types must be ints");
     static constexpr int value = sizeof...(Args);
 };
 
-int add(int a, int b)
+int fn(int a, int b)
 {
-    return a + b;
+    return 1;
+}
+
+float fn2(int a, int b)
+{
+    return 1.0;
+}
+
+float fn3(int a, float b)
+{
+    return 2.0;
 }
 
 int main(void)
 {
-    static_assert(FunctionArity<decltype(add)>::value == 2, "");
+    static_assert(FunctionArity<decltype(fn)>::value == 2, "");
+    // static_assert(FunctionArity<decltype(fn2)>::value == 2, "");
 }
