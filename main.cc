59e08989b6c3f6c9f1b925ad7d676a22c73a8aba
Daniel Alp <daniel.alp.email@gmail.com>
2026-01-11 10:44:22 -0500

    cleanup

--- main.cc
+++ main.cc
@@ -1,27 +1,19 @@
 #include <stdio.h>
 
 template <typename T, typename U>
-struct IsSame {
-    static constexpr bool value = false;
-};
+constexpr bool is_same = false;
 
 template <typename T>
-struct IsSame<T, T> {
-    static constexpr bool value = true;
-};
+constexpr bool is_same<T, T> = true;
 
 template <typename>
-struct IsValidForeignFn {
-    static constexpr bool value = false;
-};
+constexpr bool is_foreign_fn = false;
 
 template <typename R, typename... Args>
-struct IsValidForeignFn<R (*)(Args...)> {
-    static constexpr bool value = IsSame<R, int>::value && (IsSame<Args, int>::value && ...);
-};
+constexpr bool is_foreign_fn<R (*)(Args...)> = is_same<R, int> && (is_same<Args, int> && ...);
 
 template <typename>
-struct FunctionArity {};
+struct FunctionArity;
 
 template <typename R, typename... Args>
 struct FunctionArity<R (*)(Args...)> {
@@ -33,15 +25,19 @@ typedef int (*ForeignFnWrapper)(int *);
 template <auto F>
 int foreign_fn_wrapper(int *vals)
 {
-    if constexpr (!IsValidForeignFn<decltype(F)>::value) {
+    if constexpr (!is_foreign_fn<decltype(F)>) {
         static_assert(false, "expected foreign function");
     } else {
         constexpr int N = FunctionArity<decltype(F)>::value;
-        static_assert(N <= 3, "foreign function must take at most 3 arguments");
-        if constexpr (N == 0) return F();
-        if constexpr (N == 1) return F(vals[0]);
-        if constexpr (N == 2) return F(vals[0], vals[1]);
-        if constexpr (N == 3) return F(vals[0], vals[1], vals[2]);
+        static_assert(N <= 3, "foreign function take 3 arguments max");
+        if constexpr (N == 0)
+            return F();
+        if constexpr (N == 1)
+            return F(vals[0]);
+        if constexpr (N == 2)
+            return F(vals[0], vals[1]);
+        if constexpr (N == 3)
+            return F(vals[0], vals[1], vals[2]);
     }
 }
 
@@ -75,7 +71,8 @@ int main(void)
     printf("%d\n", f_fn(vals));
 
     // comment out to compile
-    make_foreign_fn<fn2>();
-    make_foreign_fn<fn3>();
-    make_foreign_fn<3>();
+    // make_foreign_fn<fn2>();
+    // make_foreign_fn<fn3>();
+    // make_foreign_fn<3>();
+    // make_foreign_fn();
 }
